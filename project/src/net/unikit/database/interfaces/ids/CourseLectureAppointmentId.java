c8acbd06965f98ebe7bccdb9023450beba89c919
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-25 17:14:35 +0100

    Removed ids because of case sensitive renaming. Will be restored in next commit.

--- project/src/net/unikit/database/interfaces/ids/CourseLectureAppointmentId.java
+++
@@ -1,9 +0,0 @@
-package net.unikit.database.interfaces.ids;
-
-/**
- * An id for a course lecture appointment.
- * @author Andreas Berks
- * @since 1.2.1
- */
-public interface CourseLectureAppointmentID extends AppointmentID, AbstractID<Integer> {
-}
--- project/src/net/unikit/database/interfaces/ids/CourseLectureId.java
+++
@@ -1,9 +0,0 @@
-package net.unikit.database.interfaces.ids;
-
-/**
- * An id for a course lecture.
- * @author Andreas Berks
- * @since 1.2.1
- */
-public interface CourseLectureID extends DidacticUnitID, AbstractID<Integer> {
-}
