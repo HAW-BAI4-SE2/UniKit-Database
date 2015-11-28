d059e62ff6fba0d8614f9bfcc42d3086c168944f
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-28 18:08:47 +0100

    Added test (draft).

---
+++ project/test/net/unikit/database/CourseManagerTest2.java
@@ -0,0 +1,47 @@
+package net.unikit.database;
+
+import junit.framework.TestCase;
+import net.unikit.database.interfaces.entities.Course;
+import net.unikit.database.interfaces.managers.CourseManager;
+import org.hibernate.exception.ConstraintViolationException;
+
+import java.util.List;
+
+/**
+ * Created by Andreas on 28.11.2015.
+ */
+public class CourseManagerTest2 extends TestCase {
+    public void testGetAllEntities() throws Exception {
+        CourseManager courseManager = TestSuite.databaseManager.getCourseManager();
+        List<Course> courses = courseManager.getAllEntities();
+        assertEquals(courses.size(), 4);
+
+        Course entity = courseManager.getEntity(new CourseImpl.IDImpl(3));
+        assertEquals(entity.getName(), "Betriebssysteme");
+        /*
+        courseManager.deleteEntity(entity);
+        assertEquals(courses.size(), 3);
+        */
+        try {
+            //entity = courseManager.getEntity(new CourseImpl.IDImpl(3));
+            courseManager.deleteEntity(entity);
+        } catch (ConstraintViolationException e) {
+            assertTrue(true);
+        }
+
+        entity = courseManager.getEntity(courseManager.createID(3));
+
+        Course entity1 = courseManager.createEntity();
+        assertEquals(courses.size(), 4);
+
+        entity1.setName("blabla");
+        entity1.setAbbreviation("BLA");
+        entity1.setCourseLecture(entity.getCourseLecture());
+        entity1.setMaxTeamSize(2);
+        entity1.setMinTeamSize(2);
+        entity1.setSemester(4);
+
+        courseManager.addEntity(entity1);
+        assertEquals(courses.size(), 5);
+    }
+}
---
+++ project/test/net/unikit/database/TestSuite.java
@@ -0,0 +1,51 @@
+package net.unikit.database;
+
+import net.unikit.database.interfaces.DatabaseConfiguration;
+import net.unikit.database.interfaces.DatabaseManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import java.io.File;
+import java.io.IOException;
+
+import static net.unikit.database.DatabaseConfigurationUtils.createDatabaseConfigurationFromProperties;
+
+/**
+ * Created by Andreas on 28.11.2015.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CourseManagerTest.class,
+        CourseManagerTest2.class
+})
+public class TestSuite {
+    static DatabaseManager databaseManager;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String externalConfigurationFile = "conf" + File.separator + "database_external.properties";
+        DatabaseConfiguration configurationExternal = null;
+        try {
+            configurationExternal = createDatabaseConfigurationFromProperties(externalConfigurationFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String internalConfigurationFile = "conf" + File.separator + "database_internal.properties";
+        DatabaseConfiguration configurationInternal = null;
+        try {
+            configurationInternal = createDatabaseConfigurationFromProperties(internalConfigurationFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        databaseManager = DatabaseManagerFactory.createDatabaseManager(configurationInternal, configurationExternal);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        databaseManager = null;
+    }
+}
